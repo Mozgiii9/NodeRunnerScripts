b670f8bff96dd07e0a7f17fa25378b86d4c250d8
Nikita Mozgin <74683169+Mozgiii9@users.noreply.github.com>
2025-04-29 15:28:46 +0300

    Create biconomy_update.sh

---
+++ biconomy_update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Определяем, какой синтаксис docker-compose доступен
+if command -v docker-compose &> /dev/null; then
+  DC="docker-compose"
+elif docker compose version &> /dev/null; then
+  DC="docker compose"
+else
+  echo "Ошибка: не найдено ни команды 'docker-compose', ни команды 'docker compose'." >&2
+  exit 1
+fi
+
+# Переходим в папку проекта
+cd mee-node-deployment
+
+# Останавливаем
+$DC down
+sleep 5
+
+# Обновляем образ в compose-файле
+sed -i -E 's#^(\s*image:\s*bcnmy/mee-node:).*#\11.1.14#' docker-compose.yml
+
+# Запускаем
+$DC up -d
+
+# Показываем логи
+docker logs -f mee-node-deployment-node-1
